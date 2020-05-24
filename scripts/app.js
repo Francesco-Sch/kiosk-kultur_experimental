886f292d32a245a68420481c05bd05cde91f7358
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2020-05-24 21:26:13 +0200

    Adds gradient to bottom of the page

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,16 @@
 /* ----- Resizes headline equally to the parent div ----- */
 fitty('#main-headline');
+
+/* ----- Activates bottom gradient after scroll ----- */
+let bottomGradient = document.querySelector('.bottom-gradient');
+let activePoint = bottomGradient.clientHeight;
+
+function activatesGradient() {
+    if(window.scrollY >= activePoint) {
+        bottomGradient.style.opacity = '1'
+    } else {
+        bottomGradient.style.opacity = '0'
+    }
+}
+
+window.addEventListener('scroll', activatesGradient);
